248be951fece2eeb9221ff8275e90f74f5d3e817
stanislavlia <stanislavliashkov@gmail.com>
2024-07-28 17:16:29 +0700

    finally ran full pipeline sucessfully

--- scripts/build_train_data.py
+++ scripts/build_train_data.py
@@ -1,30 +1,22 @@
 import polars as pl
-import numpy as np
 import click
-
+import os
 
 @click.command()
-@click.option("--merged_data_path",
-              default="../processed_data/users_data_merged.csv",
-              type=click.Path(exists=True))
-@click.option("--train_labels_path", type=click.Path(exists=True))
-@click.option("--output_dir", default="../processed_data/", type=click.Path())
-def build_train_data(merged_data_path,
-                     train_labels_path,
-                     output_dir):
+@click.option("--merged_data_path", help="Path to the merged data file")
+@click.option("--train_labels_path", help="Path to the training labels file")
+@click.option("--output_dir", default="processed_data/", help="Directory to save the output data")
+def build_train_data(merged_data_path, train_labels_path, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    merged_df = pl.read_csv(merged_data_path)
+    train_labels_df = pl.read_csv(train_labels_path)
 
-    merged_data_df = pl.scan_csv(merged_data_path)
-    train_labels_df = pl.scan_csv(train_labels_path)
-
-    train_df = train_labels_df.join(merged_data_df, on="msno", how="left")
-
-    train_df.sink_csv(output_dir + "full_train_data.csv",
-                      batch_size=100,
-                      float_precision=32,
-                      maintain_order=False,
-                      type_coercion=True,
-                      simplify_expression=True)
-
+    train_df = merged_df.join(train_labels_df, on="msno", how="left")
+    
+    output_path = os.path.join(output_dir, "full_train_data.csv")
+    train_df.write_csv(output_path)
 
 if __name__ == "__main__":
     build_train_data()
